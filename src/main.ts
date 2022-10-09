d1dd286b59ceedf8a31d134731758c3a478afaaf
mavismmg <contatomateusjorgejf@gmail.com>
2022-10-09 14:38:27 -0300

    add: config env for deploy in heroku

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  const port = 3000;
+  const port = process.env.PORT;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
